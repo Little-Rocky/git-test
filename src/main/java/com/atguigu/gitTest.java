41e8dcf0b865cfc304bb922463059813c890641e
Rocky <Rocky@qq.com>
2022-05-11 17:54:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/gitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Description:
+ * @Author: Rocky
+ * @version:
+ * @date: 2022/5/11 17:50
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,ideaGit");
+    }
+}
