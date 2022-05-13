b43e69a24dc745e10bc52f3bfd7d06ea11e58939
Little-Rocky <74861954+Little-Rocky@users.noreply.github.com>
2022-05-13 17:00:48 +0800

    pull test

--- src/main/java/com/atguigu/gitTest.java
+++ src/main/java/com/atguigu/gitTest.java
@@ -15,5 +15,6 @@ public class gitTest {
         System.out.println("master test1");
         System.out.println("hot-fix test1");
         System.out.println("push test1");
+        System.out.println("pull test1");
     }
 }
